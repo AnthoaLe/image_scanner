3e59845bf1e277a83c53d8355e0e879f10410f66
AnthoaLe <anhoal@uci.edu>
2021-08-11 16:07:20 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+
+  runApp(CameraApp());
+}
+
+class CameraApp extends StatefulWidget {
+  @override
+  _CameraAppState createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TakePicture(camera: cameras.first),
+      theme: ThemeData.dark(),
+    );
+  }
+}
+
+class TakePicture extends StatefulWidget {
+  TakePicture({Key? key, required this.camera}) : super(key: key);
+
+  final camera;
+  
+  @override
+  _TakePictureState createState() => _TakePictureState();
+}
+
+class _TakePictureState extends State<TakePicture> {
+  late CameraController controller;
+  late Future<void> initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras.first, ResolutionPreset.max);
+    initializeControllerFuture = controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Take a picture'),
+      ),
+      body: FutureBuilder<void>(
+        future: initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Center(
+              child: CameraPreview(controller),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.camera_alt),
+        onPressed: () async {
+          try {
+            await initializeControllerFuture;
+            final image = await controller.takePicture();
+            await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return DisplayPicture(
+                    imagePath: image.path,
+                  );
+                },
+              )
+            );
+          } catch (e) {
+            print(e);
+          }
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class DisplayPicture extends StatefulWidget {
+  const DisplayPicture({Key? key, required this.imagePath}) : super(key: key);
+
+  final String imagePath;
+
+  @override
+  _DisplayPictureState createState() => _DisplayPictureState();
+}
+
+class _DisplayPictureState extends State<DisplayPicture> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Display the Picture'),
+      ),
+      body: Center(
+        child: Image.file(File(widget.imagePath)),
+      ),
+    );
+  }
+}
